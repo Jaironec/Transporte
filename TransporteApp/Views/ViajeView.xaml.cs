3d55648269f4894bb6043271b97ce74915c81b5e
Jaironec <84531140+Jaironec@users.noreply.github.com>
2025-11-25 17:45:46 -0500

    Enhance viaje registration UX and resiliency

---
+++ TransporteApp/Views/ViajeView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TransporteApp.Views;
+
+public partial class ViajeView : UserControl
+{
+    public ViajeView()
+    {
+        InitializeComponent();
+    }
+}
